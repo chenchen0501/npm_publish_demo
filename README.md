796471ad06e6b231170b85bf46007c391cab3679
chenchen1415 <chenchen@wuxicloud.com>
2020-12-29 15:12:45 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# 注意点
+ *  clone本仓库至本地
+ *  npm install
+ *  修改package.json中
+    *  name - 修改为自己需要发版的npm包名字（名字不可与npm已有包名重复、名字正常一点不然发不了）
+    *  main - 根据name自行修改
+    *  author - 在npm注册的账号
+ * app.vue中的组件即为需要发版的组件
+ * index.js中导出组件
+ * 发版一定要切换npm源，不要使用淘宝源，不然发布了
+ * npm发版步骤请查看[我的简书](https://www.jianshu.com/p/999e3f85b37f)
---
+++ src/app.vue
@@ -0,0 +1,23 @@
+<template>
+  <div style="background: blue; font-weight: bold">
+    <span>hello!</span>
+    <span>{{ data.name }}</span>
+  </div>
+</template>
+<script>
+export default {
+  name: "Msg",
+  data() {
+    return {};
+  },
+  props: {
+    data: {
+      type: Object,
+    },
+  },
+  methods: {},
+  created() {
+    let that = this;
+  },
+};
+</script>
